c1a0cfeb55139b2cc9453fbca881342d0c666946
Billy Zhou <billyzhou@Billys-MBP.hsd1.ca.comcast.net>
2020-01-07 18:03:28 -0800

    changed prettier indent from tab to space

--- _coursera-algos/divide-and-conquer/count-inversions.js
+++ _coursera-algos/divide-and-conquer/count-inversions.js
@@ -30,30 +30,30 @@ Strategy
  */
 
 const countInversions = arr => {
-	let counter = 0;
-	(function mergesort(a) {
-		if (a.length < 2) return a;
-		else {
-			const mid = a.length >> 1;
-			const left = mergesort(a.slice(0, mid));
-			const right = mergesort(a.slice(mid));
-			const merge = [];
-			let l = 0,
-				r = 0;
-			while (l < left.length || r < right.length) {
-				if (left[l] < right[r] || right[r] === undefined) {
-					merge.push(left[l]);
-					l++;
-				} else {
-					merge.push(right[r]);
-					if (l < left.length) counter += left.length - l;
-					r++;
-				}
-			}
-			return merge;
-		}
-	})(arr);
-	return counter;
+  let counter = 0;
+  (function mergesort(a) {
+    if (a.length < 2) return a;
+    else {
+      const mid = a.length >> 1;
+      const left = mergesort(a.slice(0, mid));
+      const right = mergesort(a.slice(mid));
+      const merge = [];
+      let l = 0,
+        r = 0;
+      while (l < left.length || r < right.length) {
+        if (left[l] < right[r] || right[r] === undefined) {
+          merge.push(left[l]);
+          l++;
+        } else {
+          merge.push(right[r]);
+          if (l < left.length) counter += left.length - l;
+          r++;
+        }
+      }
+      return merge;
+    }
+  })(arr);
+  return counter;
 };
 
 const input = [10, 1, 2, 3, 6, 4, 5];
