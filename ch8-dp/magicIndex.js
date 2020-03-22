8b97ed9db76405b3ce893977c5c790c125d6adf6
williazz <williazz@usc.edu>
2020-03-22 16:44:37 -0700

    completed magic index

--- ch8-dp/magicIndex.js
+++ ch8-dp/magicIndex.js
@@ -3,15 +3,62 @@
  * @return {integer}
  */
 
-const magicIndex = arr => {
-  let m = [];
+// Brute force
+// O(n) time
+// o(c) space
+
+const magicIndexBF = arr => {
   for (let i = 0; i < arr.length; i++) {
-    if (i < arr[i]) return m;
-    else if (i === arr[i]) m.push(i);
+    if (i < arr[i]) return false;
+    else if (i === arr[i]) return i;
   }
-  return m;
+  return false;
 };
 
-const arr = [-5, -3, 1, 2, 5];
-const output = magicIndex(arr);
-console.log(output);
+// Recursive
+// O(logn) time
+// O(logn) space (implicit call stack)
+
+const magicIndexRec = (arr = [], left = 0, right = arr.length - 1) => {
+  if (left > right) return false;
+  const mid = (left + right) >> 1;
+  if (mid === arr[mid]) return mid;
+  if (mid < arr[mid]) return magicIndexRec(arr, left, mid - 1);
+  else return magicIndexRec(arr, mid + 1, right);
+};
+
+// Dynamic
+// O(logn) time
+// O(c) space
+
+const magicIndexDP = arr => {
+  let left = 0,
+    right = arr.length - 1;
+  while (left <= right) {
+    const mid = (left + right) >> 1;
+    if (arr[mid] === mid) return mid;
+    if (arr[mid] > mid) right = mid - 1;
+    else left = mid + 1;
+  }
+  return false;
+};
+
+const arr = [-100];
+for (let i = 0; i < 100; i++) {
+  const last = arr[arr.length - 1];
+  const next = last + Math.random() + 10;
+  arr.push(next);
+}
+
+let passes = true;
+for (let i = 0; i < 100; i++) {
+  let isEqual =
+    magicIndexDP(arr) === magicIndexRec(arr) &&
+    magicIndexDP(arr) === magicIndexBF(arr);
+
+  if (!isEqual) {
+    passes = false;
+    break;
+  }
+}
+console.log(passes);
