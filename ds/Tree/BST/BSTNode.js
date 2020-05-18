99d01413a5dbeacb4fc11902fb53af1f14e91318
williazz <williazz@usc.edu>
2020-05-18 02:03:19 -0700

    saving BST.insert before researching tree traversal

--- ds/Tree/BST/BSTNode.js
+++ ds/Tree/BST/BSTNode.js
@@ -1,15 +1,49 @@
+const Queue = require('../../Queue');
+
 /**
  * Creates a BST node
  * @class
  */
 class BSTNode {
 	constructor(val) {
+		if (typeof val !== 'number')
+			throw new Error('BSTNode must be called with a number');
+
 		this.val = val;
 		this.left = null;
 		this.right = null;
 	}
-	insert(val) {}
-	find(val) {}
+
+	/**
+	 * Inserts a value into the BST node
+	 * @param {*} val - The value to insert into the BST
+	 */
+	insert(val) {
+		let cn = this;
+		const getDir = () => (cn.val < val ? 'left' : 'right');
+		let dir = getDir();
+		while (cn[dir] instanceof BSTNode) {
+			cn = cn[dir];
+			dir = getDir();
+		}
+		cn[dir] = new BSTNode(val);
+		return cn[dir];
+	}
+
+	/**
+	 * Executes a query via breadth first search
+	 * @param {*} query
+	 */
+	searchBreadthFirst(query) {
+		const jobs = new Queue();
+		jobs.enqueue(this);
+		while (!jobs.isEmpty()) {
+			const cn = jobs.dequeue();
+			if (cn.left) jobs.enqueue(cn.left);
+			if (cn.right) jobs.enqueue(cn.right);
+			// stuff
+		}
+	}
 	update(val) {}
 	remove(val) {}
 }
--- ds/Tree/BST/README.md
+++ ds/Tree/BST/README.md
@@ -5,4 +5,4 @@
 ### Sources
 
 - [BST, Wikipedia](https://en.wikipedia.org/wiki/Binary_search_tree)
--
+- [BST Visualizer, USFCA](https://www.cs.usfca.edu/~galles/visualization/BST.html)
