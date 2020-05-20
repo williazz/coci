5fa40033cd81c9432ec94c5c6648739e1706c570
williazz <williazz@usc.edu>
2020-05-19 23:07:53 -0700

    BST.constructor
    Finally, after much over engineering, BST.constructor is a go. It accepts a val and options. By default, it is a normal BST that does not allow for duplicates. However, options can be configured to allow duplicates; and measure can be adjusted to allow for the BST to store non-numerical values

--- ds/Tree/BST/__test__/bst.test.js
+++ ds/Tree/BST/__test__/bst.test.js
@@ -2,14 +2,102 @@ const BST = require('../index.js');
 const _ = require('underscore');
 
 describe('BST', () => {
-	describe('checkIntegrity', () => {});
-	describe('isEmpty', () => {
-		const test = new BST();
-		it('should be empty at first', (done) => {
-			expect(test.isEmpty()).toBeTrue();
+	describe('constructor', () => {
+		it('should require that val is measurable', (done) => {
+			try {
+				const blank = new BST();
+				expect(true).toBeFalse();
+			} catch (err) {
+				expect(err).toBeInstanceOf(Error);
+			}
+
+			try {
+				const one = new BST(1);
+			} catch (err) {
+				expect(err).toBeUndefined();
+			}
+
+			try {
+				const word = new BST('please fail');
+				expect(true).toBeFalse();
+			} catch (err) {
+				expect(err).toBeInstanceOf(Error);
+			}
+			done();
+		});
+
+		it('should have the default options', (done) => {
+			const one = new BST(1);
+			const { duplicates, measure } = one.options;
+			expect(duplicates).toBeFalse();
+			expect(measure.toString()).toEqual(((val) => val).toString());
+			done();
+		});
+
+		it('should recognize new options', (done) => {
+			const yesDups = new BST(1, { duplicates: true });
+			const { duplicates, measure } = yesDups.options;
+			expect(duplicates).toBeTrue();
+			expect(measure.toString()).toEqual(((val) => val).toString());
+			done();
+		});
+
+		it('should allow non-number vals to be measured', (done) => {
+			try {
+				const word = new BST('harry potter!', { measure: (str) => str.length });
+			} catch (err) {
+				expect(err).toBeUndefined();
+			}
 			done();
 		});
 	});
+
+	describe('isEmpty', () => {
+		it('should be empty at first', (done) => {
+			expect(blank.isEmpty()).toBeTrue();
+			done();
+		});
+
+		it('should not be empty with a root', (done) => {
+			const root = new BST(10);
+			expect(root.isEmpty()).toBeFalse();
+			done();
+		});
+	});
+
+	describe('getDirection', () => {
+		it('should say to go right', (done) => {
+			const getDirection = blank.getDirection;
+			expect(getDirection(10, 5)).toEqual('right');
+			done();
+		});
+	});
+
+	// describe('checkIntegrity', () => {
+	// 	it('should accept a blank bst', (done) => {
+	// 		try {
+	// 			blank.checkIntegrity();
+	// 		} catch (err) {
+	// 			expect(err).toBeUndefined();
+	// 		}
+	// 		done();
+	// 	});
+
+	// 	it('should recognize an incorrect bst', (done) => {
+	// 		const five = new BST(5);
+	// 		five.left = new BST(10);
+	// 		five.right = new BST(15);
+
+	// 		try {
+	// 			five.checkIntegrity();
+	// 			expect(true).toBeFalse();
+	// 		} catch (err) {
+	// 			expect(err).toBeInstanceOf(Object);
+	// 		}
+	// 		done();
+	// 	});
+	// });
+
 	describe('add', () => {});
 	describe('getHeight', () => {});
 	describe('getNumberOfNodes', () => {});
--- ds/Tree/BST/index.js
+++ ds/Tree/BST/index.js
@@ -1,11 +1,9 @@
-const Queue = require('../../Queue');
-
 /** @class Creates a Binary Search Tree */
 class BST {
 	/**
 	 * @param {*} val - Sets the root value of this BST node
 	 * @param {Object} [options]
-	 * @param {Function} [options.measure=identity(val)] - Determines how to measure and therefore compare node values
+	 * @param {Function} [options.measure=identity(val)] - Determines how to measure and therefore compare node values. Must return a number
 	 * @param {Boolean} [options.duplicates=false] - Determines whether or not duplicate values are allowed
 	 */
 	constructor(val, options = {}) {
@@ -13,60 +11,73 @@ class BST {
 			measure: (val) => val,
 			duplicates: false,
 		};
+		this.options = Object.assign(defaultOptions, options);
+
+		const measureVal = this.options.measure(val);
+		if (typeof measureVal !== 'number')
+			throw new Error(`
+				BST val must be measurable!
+					measure(val)=${measureVal}
+					typeof=${typeof measureVal}
+			`);
+
 		this.val = val;
 		this.left = null;
 		this.right = null;
-		this.options = Object.assign(defaultOptions, options);
 
-		this.binarySearchProperty = this.binarySearchProperty.bind(this);
+		this.getDirection = this.getDirection.bind(this);
 	}
 
 	/**
-	 * Compares a new value to node value and defines the direction the new value should go
+	 * Defines the binary search property. Compares a new value to node value and defines the direction of the new value
 	 * @param {*} val
 	 * @param {*} node
-	 * @returns {String} left | right. If duplicates are not allowed, it can also return false.
+	 * @returns {String} left | right
 	 */
-	binarySearchProperty(val, nodeVal) {
+	getDirection(next, cur) {
 		const { duplicates, measure } = this.options;
-		const v = measure(val);
-		const n = measure(nodeVal);
-		if (!duplicates && v === n) return false;
-		return v < n ? 'left' : 'right';
+		next = measure(next);
+		cur = measure(cur);
+		if (!duplicates && next === cur) return;
+		return next < cur ? 'left' : 'right';
 	}
 
 	/**
 	 * Invokes a visit callback on every node value in a pre-order depth-first traversal
-	 * @callback visit - visit(node)
+	 * @param {Function} visit
 	 */
 	preOrderTraverse(visit = () => {}) {
-		visit(this.val);
-		if (this.left) this.preOrderTraverse(this.left);
-		if (this.right) this.preOrderTraverse(this.right);
+		const traverse = (node) => {
+			if (!node) return;
+			visit(node);
+			traverse(node.left);
+			traverse(node.right);
+		};
+
+		traverse(this);
 	}
 
 	/** Traverses the BST and throws error if binary search property is not satisfied */
 	checkIntegrity() {
-		const { binarySearchProperty } = this;
+		const { getDirection } = this;
+		const { measure } = this.options;
 		this.preOrderTraverse((node) => {
-			if (node === null) return;
 			// check left < parent
-			if (
-				node.left &&
-				binarySearchProperty(node.val, node.left.val) !== 'left'
-			) {
+			if (typeof measure(node.val) !== 'number')
+				throw new Error(`
+					BST node values must be measurable!
+						node.val=${node.val}
+						measure(val)=${measure(node.val)}
+				`);
+			if (node.left && getDirection(node.val, node.left.val) !== 'left') {
 				throw new Error(
 					`BST: left ${node.left.val} must be less than parent ${node.val}`,
 				);
 			}
-
-			// check parent <= right (depending on options.duplicates)
-			if (
-				node.right &&
-				binarySearchProperty(node.val, node.right.val) !== 'right'
-			)
+			// check parent < right
+			if (node.right && getDirection(node.val, node.right.val) !== 'right')
 				throw new Error(
-					`BST: left ${node.left.val} must be less than parent ${node.val}`,
+					`BST: right ${node.right.val} must be >= parent ${node.val}; duplicates=${options.duplicates}`,
 				);
 		});
 	}
@@ -101,6 +112,31 @@ class BST {
 			}
 			dir = getDirection(val, node);
 		}
+	}
+
+	contains(val) {
+		const measure = this.options.measure;
+		const getDirection = this.getDirection;
+		let cn = this;
+		let dir = getDirection(val, cn.val);
+		while (dir) {
+			cn = cn[dir];
+			dir = getDirection(val, cn.val);
+		}
+	}
+
+	getHeight() {
+		let res = 0;
+		const dfs = (node, depth = 0) => {
+			if (node === null) {
+				if (depth > res) res = depth;
+				return;
+			}
+			dfs(node.left, depth + 1);
+			dfs(node.right, depth + 1);
+		};
+		dfs(this);
+		return res;
 	}
 
 	inOrderTraverse() {}
